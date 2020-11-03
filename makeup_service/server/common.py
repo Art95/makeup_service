4d7f8f4221a7b1e3a66902967f1a5079c322a55e
Artem Baraniuk <artem.baranyuk@gmail.com>
2020-11-03 15:05:03 +0200

    Moved package related data to package

--- makeup_service/server/common.py
+++ makeup_service/server/common.py
@@ -7,7 +7,7 @@ def get_uploads_folder_path():
 
 
 def get_data_folder():
-    root_folder = Path(__file__).parent.parent.parent
+    root_folder = Path(__file__).parent.parent
     data_folder = os.path.join(root_folder, 'data')
 
     return data_folder
