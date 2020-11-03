c00fb790dce2d342511fb3adb3f348261a217ba5
Artem Baraniuk <artem.baranyuk@gmail.com>
2020-11-03 12:15:12 +0200

    Updates for install

--- setup.py
+++ setup.py
@@ -19,4 +19,11 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=[
+        'torch',
+        'torchvision',
+        'scikit-image',
+        'flask',
+        'numpy'
+    ]
 )
