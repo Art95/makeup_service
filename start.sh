b415cb0851c9ca217a8bdf0387ae07b498561ed3
Artem Baraniuk <artem.baranyuk@gmail.com>
2020-11-05 23:42:00 +0200

    Improvements in configs

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-docker run --name makeup_service --gpus all --network host -det -i -v $PWD:/app makeup_service
+docker run --name makeup_service --gpus all --network host -det -i -v "$PWD":/app abaraniuk/makeup_service:latest
 docker exec -i makeup_service bash -c "cd /app/ && pip3 install -r requirements.txt ."
-docker exec -i makeup_service sh -c "python3 /app/makeup_service/server/app.py --host 0.0.0.0 --port 5000"
+docker exec -i makeup_service bash -c "python3 /app/makeup_service/server/app.py --host 0.0.0.0 --port 5000"
