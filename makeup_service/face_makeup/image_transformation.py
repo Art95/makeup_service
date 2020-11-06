39c9f6f0222128251fda9281d66451d87a3eba51
Artem Baraniuk <artem.baranyuk@gmail.com>
2020-11-06 20:06:17 +0200

    Returned more effective approach

--- makeup_service/face_makeup/image_transformation.py
+++ makeup_service/face_makeup/image_transformation.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 from skimage.filters import gaussian
 import enum
 
@@ -29,99 +30,26 @@ def sharpen(img):
     return np.array(img_out, dtype=np.uint8)
 
 
-def color_bgr_to_hsv(color):
-    r = color[2]
-    g = color[1]
-    b = color[0]
-
-    r_t = r / 255.0
-    g_t = g / 255.0
-    b_t = b / 255.0
-
-    c_max = max(r_t, g_t, b_t)
-    c_min = min(r_t, g_t, b_t)
-
-    delta = c_max - c_min
-
-    h = calc_hue(r_t, g_t, b_t, delta, c_max)
-    s = calc_saturation(delta, c_max)
-    v = c_max
-
-    return [h, s, v]
-
-
-def calc_hue(r_t, g_t, b_t, delta, c_max):
-    hue = None
-
-    if delta == 0:
-        hue = 0
-    elif c_max == r_t:
-        hue = 60 * (((g_t - b_t) / delta) % 6)
-    elif c_max == g_t:
-        hue = 60 * (((b_t - r_t) / delta) + 2)
-    elif c_max == b_t:
-        hue = 60 * (((r_t - g_t) / delta) + 4)
-
-    return hue
-
-
-def calc_saturation(delta, c_max):
-    st = delta / c_max if c_max != 0 else 0.0
-    return st
-
-
-def color_hsv_to_bgr(color):
-    h = color[0]
-    s = color[1]
-    v = color[2]
-
-    c = v * s
-    x = c * (1 - abs((h / 60) % 2 - 1))
-    m = v - c
-
-    r_t, g_t, b_t = None, None, None
-
-    if 0 <= h < 60:
-        r_t, g_t, b_t = c, x, 0
-    elif 60 <= h < 120:
-        r_t, g_t, b_t = x, c, 0
-    elif 120 <= h < 180:
-        r_t, g_t, b_t = 0, c, x
-    elif 180 <= h < 240:
-        r_t, g_t, b_t = 0, x, c
-    elif 240 <= h < 300:
-        r_t, g_t, b_t = x, 0, c
-    elif 300 <= h < 360:
-        r_t, g_t, b_t = c, 0, x
-
-    r = round((r_t + m) * 255)
-    g = round((g_t + m) * 255)
-    b = round((b_t + m) * 255)
-
-    return [int(b), int(g), int(r)]
-
-
 def change_segment_color(image, segmentation, head_part, color):
-    segment_indexes = np.where(segmentation == head_part.value)
-    segment_pixels = list(zip(segment_indexes[0], segment_indexes[1]))
+    b, g, r = color
+    tar_color = np.zeros_like(image)
+    tar_color[:, :, 0] = b
+    tar_color[:, :, 1] = g
+    tar_color[:, :, 2] = r
 
-    new_hsv_color = color_bgr_to_hsv(color)
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    tar_hsv = cv2.cvtColor(tar_color, cv2.COLOR_BGR2HSV)
 
-    for pixel_index in segment_pixels:
-        orig_bgr_color = image[pixel_index[0], pixel_index[1]]
-        hsv_color = color_bgr_to_hsv(orig_bgr_color)
+    if head_part is HeadPart.upper_lip or head_part is HeadPart.lower_lip:
+        image_hsv[:, :, 0:2] = tar_hsv[:, :, 0:2]
+    else:
+        image_hsv[:, :, 0:1] = tar_hsv[:, :, 0:1]
 
-        if head_part is HeadPart.upper_lip or head_part is HeadPart.lower_lip:
-            hsv_color[0:2] = new_hsv_color[0:2]
-        else:
-            hsv_color[0:1] = new_hsv_color[0:1]
+    changed = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
 
-        image[pixel_index[0], pixel_index[1]] = color_hsv_to_bgr(hsv_color)
+    if head_part is HeadPart.hair:
+        changed = sharpen(changed)
 
-    if HeadPart.hair == head_part:
-        sharpened_image = sharpen(image)
-        sharpened_image[segmentation != HeadPart.hair.value] = image[segmentation != HeadPart.hair.value]
+    changed[segmentation != head_part.value] = image[segmentation != head_part.value]
 
-        return sharpened_image
-
-    return image
+    return changed
--- tests/face_makeup/test_image_transformation.py
+++ tests/face_makeup/test_image_transformation.py
@@ -1,4 +1,3 @@
-import cv2
 import os
 from makeup_service.face_makeup.image_transformation import *
 from helpers.utils import get_test_files_folder_path
@@ -12,29 +11,6 @@ def test_head_part_values():
     assert expected_hair_value == HeadPart.hair.value
     assert expected_upper_lip_value == HeadPart.upper_lip.value
     assert expected_lower_lip_value == HeadPart.lower_lip.value
-
-
-def test_color_bgr_to_hsv():
-    bgr_colors = [(0, 0, 255), (0, 255, 0), (255, 0, 0), (0, 0, 0), (255, 255, 255), (128, 128, 128), (35, 110, 95)]
-
-    expected_hsv_values = [(0, 1.0, 1.0), (120, 1.0, 1.0), (240, 1.0, 1.0), (0, 0.0, 0.0), (0, 0.0, 1.0),
-                           (0, 0.0, 0.502), (72, 0.682, 0.431)]
-
-    for i, bgr in enumerate(bgr_colors):
-        actual_hsv_value = color_bgr_to_hsv(bgr)
-        assert np.allclose(expected_hsv_values[i], actual_hsv_value, atol=0.001)
-
-
-def test_color_hsv_to_bgr():
-    hsv_values = [(0, 1.0, 1.0), (120, 1.0, 1.0), (240, 1.0, 1.0), (0, 0.0, 0.0), (0, 0.0, 1.0),
-                  (0, 0.0, 0.502), (72, 0.682, 0.431)]
-
-    expected_bgr_values = [(0, 0, 255), (0, 255, 0), (255, 0, 0), (0, 0, 0), (255, 255, 255), (128, 128, 128),
-                           (35, 110, 95)]
-
-    for i, hsv in enumerate(hsv_values):
-        actual_bgr_value = color_hsv_to_bgr(hsv)
-        assert np.allclose(expected_bgr_values[i], actual_bgr_value)
 
 
 def test_change_segment_color():
